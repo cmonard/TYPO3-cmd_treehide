8f54cb0ee3832f53aaaa7fdc03845d5f061230eb
Christophe Monard <contact@cmonard.fr>
2014-12-19 09:22:57 +0100

    update emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,44 +3,49 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cmd_treehide".
  *
- * Auto generated 18-12-2014 18:10
+ * Auto generated 19-12-2014 09:13
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'CMD Recursive tree hider',
 	'description' => 'Enable the possibility to (un)hide an entire tree at once',
 	'category' => 'be',
-	'author' => 'Christophe Monard',
-	'author_email' => 'contact@cmonard.fr',
-	'shy' => '',
-	'dependencies' => 'cms',
+	'shy' => false,
+	'version' => '1.0.0',
+	'dependencies' => 'cms,php,typo3',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => NULL,
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
+	'uploadfolder' => false,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
+	'clearcacheonload' => true,
 	'lockType' => '',
+	'author' => 'Christophe Monard',
+	'author_email' => 'contact@cmonard.fr',
 	'author_company' => '',
-	'version' => '1.0.0',
-	'constraints' => array(
-		'depends' => array(
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'typo3' => '4.5.0-6.2.99',
 			'php' => '5.3.7-5.4.99',
-			'cms' => '',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
-	),
-	'suggests' => array(
 	),
 );
+
+?>
